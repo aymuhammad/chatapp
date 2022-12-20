29cda5f79d19a302a2e86b52ac77617856a43b14
Auwal Yahaya <auwalyah@yahoo.com>
2022-12-20 21:06:22 +0100

    add routing thesame as urls.py in django

---
+++ ontime/config/routing.py
@@ -0,0 +1,8 @@
+# it set variable of urlpatterns just like you'd normally use in django urls.py
+# the config routing in asgi.py would work correctly 
+
+from django.urls import path
+
+websocket_urlpatterns = [
+
+]
